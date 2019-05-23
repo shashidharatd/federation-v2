d74df1f71c068c32497b327f2d3d46374a446cc5
shashidharatd <shashidhara.td@huawei.com>
2019-05-23 16:23:36 +0530

    Rework on override example

--- scripts/demo/03.override.sh
+++ scripts/demo/03.override.sh
@@ -13,7 +13,10 @@ TEST_NS="demo"
 
 # Override example
 echo
-echo "Updating override of federated deployment nginx to increase 'replicas' to 2 in cluster2."
-run "kubectl patch federateddeployment nginx -n ${TEST_NS} --type=merge --patch '{\"spec\" : {\"overrides\": [{\"clusterName\" : \"cluster2\", \"clusterOverrides\": [{\"path\": \"spec.replicas\", \"value\" : 2}]}]}}'"
-run "kubectl --context cluster2 -n ${TEST_NS} get deployment nginx"
+run "kubectl patch federatedconfigmap web-file -n ${TEST_NS} --type=merge --patch '{\"spec\" : {\"overrides\": [{\"clusterName\" : \"cluster2\", \"clusterOverrides\": [{\"path\": \"data.content\", \"value\" : \"HelloKubeCon!\"}]}]}}'"
 
+echo
+IP2=$(kubectl  --context cluster2 get node -o jsonpath="{.items[0].status.addresses[0].address}")
+PORT2=$(kubectl --context cluster2 -n ${TEST_NS} get service -o jsonpath="{.items[0].spec.ports[0].nodePort}")
+
+run "curl ${IP2}:${PORT2}"
